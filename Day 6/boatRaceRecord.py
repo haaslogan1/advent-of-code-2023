40b525511332d2b68a80109f1e1bb01de0542567
Logan Haas <haaslogan1@gmail.com>
2023-12-28 15:57:01 -0500

    Day 6, puzzle 1. O(n^2). I know I can improve by elminating ranges rather than searching through every possible amount of time spent pressing the button.

---
+++ Day 6/boatRaceRecord.py
@@ -0,0 +1,37 @@
+import re
+
+# Open the input file
+content =  open('input.txt')
+
+
+# Create the time and dist array
+time = re.findall(r'\d+', content.readline())
+dist = re.findall(r'\d+', content.readline())
+
+# Create the races array from the time and dist arrays
+races = [[time[i], dist[i]] for i in range(0,4,1)]
+
+# Set the counter
+coutn = 1
+
+for i in races:
+	
+	# Extract the time and dist
+	time = int(i[0])
+	dist = int(i[1])
+	
+	
+	# Set the tmp counter
+	tmpcoutn = 0
+	
+	# iterate through the range
+	for x in range(1, time - 1):
+		# Add a value if pressing the button for time x will beat the record
+		if (x * (time - x) > dist):
+			tmpcoutn += 1
+	
+	# Multiply by ways to win this race to get margin of error
+	coutn *= tmpcoutn
+
+# Print margin or error
+print(coutn)
