1c42443e43dc5d29e56ef7e7f46983549bd913ad
Logan Haas <haaslogan1@gmail.com>
2024-01-02 19:38:54 -0500

    Working for the large input with no hardcoding! :)

--- Day 11/puzzle1.py
+++ Day 11/puzzle1.py
@@ -1,9 +1,9 @@
-l = ['...#......', '.......#..', '#.........', '..........', '......#...', '.#........', '.........#', '..........', '.......#..', '#...#.....']
+#l = ['...#......', '.......#..', '#.........', '..........', '......#...', '.#........', '.........#', '..........', '.......#..', '#...#.....']
 
-for x in l:
-	print(x)
-print()
-# ....#.........
+# Read in the actual input (smaller sample input above)
+l = [x for x in open('input.txt').read().strip('\n\n').split('\n')]
+
+
 x = []
 
 # Horizontally expand the universe
@@ -14,66 +14,70 @@ for y in range(1, len(l) + 1):
 	if '#' not in l[y - 1]:
 		x.append([y-1 , str(l[y - 1])])
 
+# Use the x list from above to shift the universe horizontally
 r = 0
 for y in x:
 	l.insert(y[0] + r, y[1])
 	r += 1
 
-# ...........#......
-
 
 x = []
 # Veritcally expand the universe
 for y in range(0, len(l[0])):
+	
+	# boolean indicator of whether or not a galaxy was found in the column
 	boo = 1
+	
 	for z in range(0, len(l)):
 		
-		# check 
+		# check for a galaxy
+		# if there is a galaxy ('#'), move on to the next line
 		if '#' == list(l[z])[y]:
 			boo = 0
 			break
 			
-			
-	if boo:
-		
+	# if there was no galaxy in the column, add it to the list of empty columns
+	if boo:	
 		x.append(y)
 
 	
+# Double each column that was found in the x list above
 r = 0
 for y in x:
 	
+	# itearate through each row of the universe
 	for z in range(0, len(l)):
 	
+		# add the extra . for each column index (y) of each row (z)
 		st_list = list(l[z])
 		
 		st_list.insert(y + r, '.')
 		
 		l[z] = ''.join(st_list)
 		
+	# iterate to account for the shifted universe
 	r += 1
 
 	
-for x in l:
-	print(x)
 		
 x = []
-# find each galaxy
+# find each galaxy iterating through every column index of every row
 for z in range(0,len(l)):
 	for y in range(0, len(l[z])):
 		
+		# if there is a galaxy at this location, add it to the list of galaxies
 		if l[z][y] == '#':
 			x.append([z, y])
 
 # print(x)
-# find longest distance
+# find total distance of each short distance
 maxDist = 0
+# iterate through every galaxy y
 for y in range(0,len(x)):
+	# iterate through every galaxy with a higher x,y value than y
 	for z in range(y + 1, len(x)):
-# 		if ((abs(x[z][0] - x[y][0]) + abs(x[z][1] - x[y][1])) > maxDist):
-		print(str(x[y]) + ' ' + str(x[z]))
-		
-		dist = abs(x[z][0] - x[y][0]) + abs(x[z][1] - x[y][1])
-		print(dist)
-		maxDist += dist	
+
+		# add the shortest possible distance (absolute value of x and y differences) to the total dist
+		maxDist += abs(x[z][0] - x[y][0]) + abs(x[z][1] - x[y][1])	
 		
 print(maxDist)
