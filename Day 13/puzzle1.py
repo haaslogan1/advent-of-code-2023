71a97a0ff7fb03a83aa63d2ad6b44a4d7f8c725f
Logan Haas <haaslogan1@gmail.com>
2024-01-05 13:13:31 -0500

    Working for the example!

--- Day 13/puzzle1.py
+++ Day 13/puzzle1.py
@@ -47,32 +47,60 @@ for x in l:
 	
 	# Check for vertical lines
 	tst = x[0]
-	
-	# iterate through each index where there could be a line
-	for num in range(1, len(tst)):
-		try:
-			if (tst[0:num] == tst[num:(2*num)]):
-				lineCnt = num
-				for y in x:
-					if (y[0:num] != y[num:(2*num)]):
-						lineCnt = 0
+
+	for y in range(1, len(tst)):
+		y = int(y)
+		if (y > (len(tst) / 2)):
+			print(tst[y:int(len(tst))])
+			print(tst[int((2 * (y - (int(len(tst)) / 2)))):y])
+			if (str(tst[y:int(len(tst))])[::-1] == str(tst[int((2 * (y - (int(len(tst)) / 2)))):y])):
+				tot = y
+				for z in x:
+					print(z[y:len(tst)])
+					print(z[int((2 * (y - (len(tst) / 2)))):y])
+					if (str(z[y:len(tst)])[::-1] != str(z[int((2 * (y - (len(tst) / 2)))):y])):
+						tot = 0
 						break
-				
-				coutn += lineCnt
-					
-		except: pass
+				coutn += tot
+		elif (y <= (len(tst) / 2)):
+			if(str(tst[0:y])[::-1] == str(tst[y:(y * 2)])):
+				tot = y
+				for z in x:
+					if(str(z[0:y])[::-1] != str(z[y:(y * 2)])):
+						tot = 0
+						break
+				coutn += tot
 		
-		try:
-			if(tst[(len(tst) - 1 - num):num] == tst[((len(tst) - 1 - num) * 2):(num / 2)]):
-				lineCnt = num - (len(tst) - 1 - num)
-				for y in x:
-					if (y[(len(y) - 1 - num):num] != y[((len(y) - 1 - num) * 2):(num / 2)]):
-						lineCnt = 0
-						break
-				
-				coutn += lineCnt
-				
-		except: pass
+		print()
+		
+		
+# 		print(tst[0:num])
+# 		try:
+# 			if (tst[0:num] == tst[num:(2*num)]):
+# 				print(tst[0:num])
+# 				lineCnt = num
+# 				for y in x:
+# 					if (y[0:num] != y[num:(2*num)]):
+# 						lineCnt = 0
+# 						break
+# 				
+# 				coutn += lineCnt
+# 					
+# 		except: pass
+# 		
+# 		try:
+# 			if(tst[(len(tst) - 1 - num):num] == tst[((len(tst) - 1 - num) * 2):(num / 2)]):
+# 				print(tst[(len(tst) - 1 - num):num])
+# 				lineCnt = num - (len(tst) - 1 - num)
+# 				for y in x:
+# 					if (y[(len(y) - 1 - num):num] != y[((len(y) - 1 - num) * 2):(num / 2)]):
+# 						lineCnt = 0
+# 						break
+# 				
+# 				coutn += lineCnt
+# 				
+# 		except: pass
+# 		print()
 
 print(coutn)
 				
