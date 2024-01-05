9fb948deb43aabaa1fd6e749981082551f3228af
Logan Haas <haaslogan1@gmail.com>
2024-01-05 16:17:57 -0500

    Puzzle 2 is working for the large input file. Still O(n^3). However, with the breaks in every for loop - the script is still performing fairly fast.

--- Day 13/puzzle2.py
+++ Day 13/puzzle2.py
@@ -1,16 +1,16 @@
 # commented l is used for the sample
 
-l = [['#.##..##.', '..#.##.#.', "##......#", "##......#", "..#.##.#.", "..##..##.", "#.#.##.#."],\
-["#...##..#",\
-"#....#..#",\
-"..##..###",\
-"#####.##.",\
-"#####.##.",\
-"..##..###",\
-"#....#..#"]]
+# l = [['#.##..##.', '..#.##.#.', "##......#", "##......#", "..#.##.#.", "..##..##.", "#.#.##.#."],\
+# ["#...##..#",\
+# "#....#..#",\
+# "..##..###",\
+# "#####.##.",\
+# "#####.##.",\
+# "..##..###",\
+# "#....#..#"]]
 
 # split the file into a list of strings separated by each new line
-# l = [x.split('\n') for x in open('input.txt').read().split('\n\n')]
+l = [x.split('\n') for x in open('input.txt').read().split('\n\n')]
 
 # initialize the count
 coutn = 0
@@ -35,25 +35,33 @@ for x in l:
 		# while we are still in the range of the list
 		while smallNum > -1 and num < len(x):
 		
-			# comparison strings
+			# comparison strings for simplicity
 			a = x[smallNum]
 			b = x[num]
 			
-			
-			for mstk in range(0, len(a)):
-				if (list(a)[mstk] != list(b)[mstk]):
-					mistakes += 1
-					if mistakes > 1:
-						lineCount = 0
-						break
-
+			# there will be one '\n' at the end of the l list
+			# just using try/except to bypass accounting for that
+			try:
+				# iterate through the length of a to count unequal chars ("mistakes")
+				for mstk in range(0, len(a)):
+					# check for unequal characters
+					if (list(a)[mstk] != list(b)[mstk]):
+						mistakes += 1
+						# there can only be one mistake!
+						if mistakes > 1:
+							lineCount = 0
+							break
+			except: pass
 				
+			# iterate the top and bottom row indeces
 			smallNum -= 1
 			num += 1
 			
+			# save time, if there is already more than one mistake
 			if mistakes > 1:
 				break
-			
+		
+		# we only add to the total count if there is one smudge ("mistake")
 		if mistakes == 1:
 			# add to the counter
 			coutn += (100 * lineCount)
@@ -78,12 +86,18 @@ for x in l:
 				a = str(z[y:len(tst)])[::-1]
 				b = str(z[int((2 * (y - (len(tst) / 2)))):y])
 				
+				# iterate through all characters of the comparison strings
 				for mstk in range(0, len(a)):
+					# check for a smudge
 					if (list(a)[mstk] != list(b)[mstk]):
 						mistakes += 1
+						
+						# there can only be one smudge!
 						if mistakes > 1:
 							tot = 0
 							break
+				# there can only be one smudge!
+				# save time by breaking the loop
 				if mistakes > 1:
 					break
 			
@@ -98,15 +112,21 @@ for x in l:
 				a = str(z[0:y])[::-1]
 				b = str(z[y:(y * 2)])
 				
+				# iterate through all characters of the comparison strings
 				for mstk in range(0, len(a)):
+					# check for a smudge
 					if (list(a)[mstk] != list(b)[mstk]):
 						mistakes += 1
+						# there can only be one smudge!
 						if mistakes > 1:
 							tot = 0
 							break
+				# there can only be one smudge!
+				# save time by breaking the loop
 				if mistakes > 1:
 					break
 			
+			# there must be exactly one smudge!
 			if mistakes == 1:
 				coutn += tot
 
