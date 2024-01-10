688a92106bc48e4047138e04bab112e8c0039b4b
Logan Haas <haaslogan1@gmail.com>
2024-01-09 20:38:06 -0500

    Day 15 Puzzle 1 implemented as a linked list. O(n^2) working for the sample.

--- Day 14/puzzle1.py
+++ Day 14/puzzle1.py
@@ -1,16 +1,59 @@
-l = ["OOOO.#.O..",\
-"OO..#....#",\
-"OO..O##..O",\
-"O..#.OO...",\
-"........#.",\
-"..#....#.#",\
-"..O..#.O.O",\
-"..O.......",\
+l = ["O....#....",\
+"O.OO#....#",
+".....##...",
+"OO.#O....O",
+".O.....O#.",
+"O.#..O.#.#",
+"..O..#O..O",
+".......O..",\
 "#....###..",\
-"#....#...."]
+"#OO..#...."]
+
+# open the file as a list of each row
+# l = open('input.txt').read().strip('\n\n').split('\n')
 
 import re
 
+
+# shift all rocks up
+# traverse from the bottom of the list to the top
+for y in range(0, len(l[0])):
+	# traverse through each character of the row
+	for x in range(1, len(l)):
+	
+		# check for the possibility to move up
+		if  list(l[x])[y] == 'O' and list(l[x - 1])[y] == '.':
+			
+			idx = x
+			
+			while idx > -1:
+				if list(l[idx - 1])[y] != '.':
+					break
+			
+				print(str(y) + ' ' + str(x))
+				print(l[idx - 1])
+				print(l[idx])
+				print()
+				tmp1 = list(l[idx])
+				tmp1[y] = '.'
+			
+				tmp2 = list(l[idx - 1])
+				tmp2[y - 1] = 'O'
+				
+			
+				l[idx] = ''.join(tmp1)
+				l[idx - 1] = ''.join(tmp2)
+				
+				print(l[idx - 1])
+				print(l[idx])
+				print()
+				print()
+				
+				idx -= 1
+
+
+for x in l:
+	print(x)
 
 # current iteration value
 itera = len(l)
---
+++ Day 15/puzzle1.py
@@ -0,0 +1,72 @@
+﻿# implementing as a linked list for basic data structure practice
+
+
+i = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7".split(',')
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+    def insertAtBegin(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+
+
+
+    def insertAtEnd(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+ 
+        current_node = self.head
+        while(current_node.next):
+            current_node = current_node.next
+ 
+        current_node.next = new_node
+
+    def hashAlgorithm(self):
+        tot = 0
+        
+        # set the current node at the beginning of the list
+        current_node = self.head
+        
+        
+        
+        # iterate until the current node becomes None
+        while(current_node):
+            # calculate a value for each character in the string
+            
+            print(current_node.data)
+            curr = 0
+            for c in list(current_node.data):
+                curr = ((curr + ord(c)) * 17) % 256
+                print(c)
+                print(str(((ord(c) * 17) % 256)))
+            current_node = current_node.next
+            print()
+            tot+= curr
+        
+        print(tot)
+
+# create the list
+llist = LinkedList()
+
+# append each value to the list
+for x in i:
+     llist.insertAtEnd(x)
+
+llist.hashAlgorithm()
